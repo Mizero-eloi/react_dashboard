ed40533d3c7ade087a7e79ccc4920e092fa5b17b
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-09-19 00:32:02 +0200

    finished orders  page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Sort,
+  Page,
+  Search,
+  Toolbar,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Employees = () => {
-  return <div>Employees</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={["Search"]}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+
+        <Inject services={[Search, Page, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Employees;
