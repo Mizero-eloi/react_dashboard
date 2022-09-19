09f82cf7de3dd6d3a4c785a5f1cd2377077bc78a
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-09-19 06:56:38 +0200

    finished Editor  page

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -11,7 +11,7 @@ import { Header } from "../components";
 const Kanban = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="App" title="Calendar" />
+      <Header category="App" title="Kanban" />
       <KanbanComponent
         id="kanban"
         dataSource={kanbanData}
