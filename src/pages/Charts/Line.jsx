b8a92fb763d5b0dddf62912b751c6b66adfdd3c6
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-09-23 00:52:45 +0200

    ready to deploy

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Line = () => {
-  return <div>Line</div>;
-};
+import { ChartsHeader, LineChart } from "../../components";
+import Header from "./../../components/Header";
+
+const Line = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    {/* <ChartsHeader category="Line" title="Inflation Rate" /> */}
+    <Header category="Chart" title="Inflation Rate" />
+    <div className="w-full">
+      <LineChart />
+    </div>
+  </div>
+);
 
 export default Line;
